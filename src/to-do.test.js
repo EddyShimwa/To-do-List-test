37dec32b0178009d2b66ebb074c10cf7095b7c10
EddyShimwa <shedpray16@gmail.com>
2022-12-16 19:51:29 +0200

    solved linter errors

--- src/to-do.test.js
+++ src/to-do.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
 
+import Todos from './modules/todosfiles.js';
+
 document.body.innerHTML = `<body>
 <main class="container">
   <section class="todo-section">
@@ -17,46 +19,44 @@ document.body.innerHTML = `<body>
 </main>
 </body>`;
 
-import Todos from "./modules/todosfiles.js";
+const domInput = document.getElementById('description');
 
-const domInput = document.getElementById("description");
-
-describe("Add Task Test ", () => {
+describe('Add Task Test ', () => {
   const list = new Todos();
-  test("Add normal task", () => {
-    domInput.value = "task 1";
+  test('Add normal task', () => {
+    domInput.value = 'task 1';
     list.onSubmit();
     expect(list.todos).toHaveLength(1);
   });
 
-  test("Add second task", () => {
-    domInput.value = "task 2";
+  test('Add second task', () => {
+    domInput.value = 'task 2';
     list.onSubmit();
     expect(list.todos).toHaveLength(2);
   });
 
-  test("Add second task", () => {
-    domInput.value = "task 3";
+  test('Add second task', () => {
+    domInput.value = 'task 3';
     list.onSubmit();
     expect(list.todos).toHaveLength(3);
   });
 });
 
-describe("Delete a todo task ", () => {
+describe('Delete a todo task ', () => {
   const list = new Todos();
-  test("remove first task", () => {
-    domInput.value = "task 1";
+  test('remove first task', () => {
+    domInput.value = 'task 1';
     list.delete();
     expect(list.todos).toHaveLength(0);
   });
 
-  test("remove second task", () => {
-    domInput.value = "task 2";
+  test('remove second task', () => {
+    domInput.value = 'task 2';
     list.delete();
     expect(list.todos).toHaveLength(0);
   });
-  test("remove the third task", () => {
-    domInput.value = "task 3";
+  test('remove the third task', () => {
+    domInput.value = 'task 3';
     list.delete();
     expect(list.todos).toHaveLength(0);
   });
